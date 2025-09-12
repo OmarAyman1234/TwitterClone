c2049a4c0ffce139ddc3d4e3e639d9af0e063a3d
OmarAyman1234 <omarayman9124@gmail.com>
2025-09-12 20:07:00 +0300

    login functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -64,7 +64,35 @@ const signup = async (req, res) => {
 }
 
 const login = async (req, res) => {
+  try {
+    const { username, password} = req.body;
+    const user = await User.findOne({username});
 
+    if(!user) {
+      return res.status(400).json({error: "Invalid username or password"});
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if(!isPasswordCorrect) {
+      return res.status(400).json({error: "Invalid username or password"});
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(201).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      email: user.email,
+      followers: user.followers,
+      following: user.following,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg,
+    })
+  } catch (error) {
+        console.log(`Error in login controller: ${error}`);
+    res.status(500).json({error: "Internal Server Error"});
+  }
 }
 
 const logout = async (req, res) => {
