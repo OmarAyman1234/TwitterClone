0b1a401af06183f20b90b212f2cbeff2a506d5dd
OmarAyman1234 <omarayman9124@gmail.com>
2025-09-12 20:14:21 +0300

    logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -90,13 +90,27 @@ const login = async (req, res) => {
       coverImg: user.coverImg,
     })
   } catch (error) {
-        console.log(`Error in login controller: ${error}`);
+    console.log(`Error in login controller: ${error}`);
     res.status(500).json({error: "Internal Server Error"});
   }
 }
 
 const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", {maxAge: 0});
+    res.status(200).json({message: "Logged out successfully"});
+  } catch (error) {
+    console.log(`Error in logout controller: ${error}`);
+    res.status(500).json({error: "Internal Server Error"});
+  }
+}
 
+export const getMe = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
 }
 
 export default {
